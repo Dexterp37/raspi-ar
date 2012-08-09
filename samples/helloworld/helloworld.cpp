d75d305272ced8edce1da995c0f4d964f370ff75
Dexter <a.placitelli@a2p.it>
2012-08-09 13:07:54 +0200

    Added missing helloworld.cpp file for the sample application

---
+++ samples/helloworld/helloworld.cpp
@@ -0,0 +1,13 @@
+/**
+ *  Author: Alessio Placitelli
+ *  Contact: a.placitelli _@_ a2p.it
+ *  
+ */
+ 
+#include <iostream>
+
+int main(int argc, char **argv) {
+    std::cout << "Hello world! Using cmake." << std::endl;
+    
+    return 0;
+}
